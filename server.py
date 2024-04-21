db97f7c4f253051423ebe1444ca05825a0e610f8
Noyo0 <norbert.antaloutlook.com>
2024-04-21 10:31:43 +0100

    flask server

---
+++ server.py
@@ -0,0 +1,19 @@
+# Web service for BigProject (Truckwash)
+# Author: Norbert Antal
+
+from flask import Flask
+import time
+
+def now():
+        current_timestamp = time.time()
+        current_datetime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(current_timestamp))
+        return(current_datetime)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+        return f"Hello world...     {now()}"
+
+if __name__ == "__main__":
+    app.run(debug = True)
