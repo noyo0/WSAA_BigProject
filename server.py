addbdcd770068b67a3bf7b14ae5630b1d5ecdcd6
Noyo0 <norbert.antaloutlook.com>
2024-04-23 20:32:11 +0100

    washlist populates from mysql.

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from flask import jsonify
 import time
 from twDAO import truckwashDAO as DAO
 
-def now():
+def now(): #get timestamp
         current_timestamp = time.time()
         #current_timestamp += 3600
         current_datetime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(current_timestamp))
@@ -14,31 +14,29 @@ def now():
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # this is the index page
 def index():
         return send_from_directory("C:/Users/norbe/OneDrive/ATU_Galway/WSAA_BigProject/WSAA_BigProject/","index.html")
         #return f"Hello world...     {now()}"
 
-@app.route('/img/<filename>')
+@app.route('/img/<filename>') #this is to display images from the img folder
 def img(filename):
         img_dir ="C:/Users/norbe/OneDrive/ATU_Galway/WSAA_BigProject/WSAA_BigProject/img"
         return send_from_directory(img_dir,filename)
 
-@app.route('/getall', methods=['GET'])
+@app.route('/getall', methods=['GET']) # this is getting mysql data for the washlikst at @/wash
 def getall():
     data = DAO.getAll()
-    # Format the date strings to short date format (e.g., '20/04/24')
+    # Format the date to (06/Jun/24)
     for entry in data:
         entry['Date'] = entry['Date'].strftime('%d/%b/%Y')  # Assuming 'Date' is a datetime object
     return jsonify(data)
 
 
-@app.route('/wash', methods=['GET'])
+@app.route('/wash', methods=['GET']) # this will list all the washes
 def washlist():
         dir ="C:/Users/norbe/OneDrive/ATU_Galway/WSAA_BigProject/WSAA_BigProject/"
         return send_from_directory(dir, 'washlist.html')
-        #img_dir ="C:/Users/norbe/OneDrive/ATU_Galway/WSAA_BigProject/WSAA_BigProject/img"
-        #return send_from_directory(img_dir,'wash_list.html', data=data)
         
 if __name__ == "__main__":
     app.run(debug = True)
